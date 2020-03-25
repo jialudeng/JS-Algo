f66230d1d16ace11c85938a41e2d9c1037840aea
Jialu Deng <jialudeng1009@gmail.com>
2020-03-24 17:29:53 -0700

    selection sort

---
+++ selectionSort.js
@@ -0,0 +1,16 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIdx = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIdx]) {
+        minIdx = j;
+      }
+    }
+    if (minIdx !== i) {
+      [arr[i], arr[minIdx]] = [arr[minIdx], arr[i]];
+    }
+  }
+  return arr;
+}
+
+console.log(selectionSort([4, 2, 3, 6, 1]))
